5cdb07bb22c0de5f3b0aeed3cfd32bf167bb92bd
Kovács Dávid <d.david010313@gmail.com>
2021-09-22 17:54:07 +0200

    unfinished

---
+++ drills/drill4/drill4.cpp
@@ -0,0 +1,16 @@
+#include "../std_lib_facilities.h"
+int main()
+{
+	//1
+	int n;
+	int m;
+	char a;
+	while (cin.get()!='|')
+	{
+		cout << "Please add 2 numbers: ";
+		cin >> n >> m;
+		cout << n <<"\n" << m <<"\n";
+		
+	}
+	return 0;
+}
