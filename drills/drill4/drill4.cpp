7607f6858ab3d2451559223ff3f1bc0ca7596dbe
Kovács Dávid <d.david010313@gmail.com>
2021-10-06 11:28:44 +0200

    first part

--- drills/drill4/drill4.cpp
+++ drills/drill4/drill4.cpp
@@ -2,15 +2,45 @@
 int main()
 {
 	//1
-	int n;
-	int m;
-	char a;
-	while ()
+	double n;
+	double m;
+	char unit=' ';
+	double few =1.0/100;
+	double diff;
+	while (unit!='|')
 	{
+		unit = ' ';
 		cout << "Please add 2 numbers: ";
 		cin >> n >> m;
-		cout << n <<"\n" << m <<"\n";
+		if (n>m)
+		{
+			cout << "The smaller value is: " << m << endl;
+			cout << "The larger value is: " << n <<endl;
+			diff = n-m << endl;
+		}
+		else if(m>n)
+		{
+			cout << "The smaller value is: " << n <<endl;
+			cout << "The larger value is: " << m <<endl;
+			diff = m-n << endl;
+		}
+
+		else
+		{
+			cout << "The numbers are equal" << endl;
+		}
+
+		if(diff<few)
+		{
+			cout << "The numbers are almost equal\n";
+		}
 		
+		
+		while (unit!='y' && unit!='|')
+		{
+			cout << "Do you want to continue? (yes = y, No = |)\n";
+			cin >> unit;
+		}
 	}
 	return 0;
 }
